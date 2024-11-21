a8379944d666ff03a778822435b25ac33b6b9023
JorgeManu <102070399+JorgeManu@users.noreply.github.com>
2024-11-21 14:34:47 -0500

    Update deploy-batch.sh

--- deploy-batch.sh
+++ deploy-batch.sh
@@ -6,7 +6,20 @@ echo "🚀 Iniciando despliegue..."
 # Clonar el repositorio
 git clone https://github.com/JorgeManu/Lab12 && cd Lab12
 
-# Desplegar con docker-compose
-docker-compose up -d
+sudo apt update
 
+sudo apt install -y docker.io
+
+sudo systemctl enable docker
+
+sudo systemctl start docker
+
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+# Desplegar con docker-compose
+sudo docker-compose up -d
+sudo docker-compose up -d
 echo " Proyecto desplegado!"
